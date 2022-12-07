d971c867e77f766cd2fad3745c488d634354324b
RafaelHDSV <rafaebolt@gmail.com>
2022-12-07 13:11:36 -0300

    Adding hamburguer

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 let header = document.querySelector('header')
+let itemsMobile = document.querySelector('.items_container_mobile')
+let hamburguer = document.getElementById('hamburguer')
 let sliderHeader = ['slider_header1.jpg', 'slider_header2.jpg', 'slider_header3.jpg', 'slider_header4.jpg', 'slider_header5.jpg']
 let i = 0
 
@@ -18,3 +20,11 @@ const typed = new Typed('.auto_type', {
     backSpeed: 100,
     loop: true
 });
+
+hamburguer.addEventListener('click', () => {
+    if (itemsMobile.style.display == 'none') {
+        itemsMobile.style.display = 'flex'
+    } else {
+        itemsMobile.style.display = 'none'
+    }
+})
