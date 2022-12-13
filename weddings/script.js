3750e8451c888c167aabd0a73129d1a9c76af7c6
RafaelHDSV <rafaebolt@gmail.com>
2022-12-13 11:02:34 -0300

    Updating prices

--- weddings/script.js
+++ weddings/script.js
@@ -1,6 +1,12 @@
 let preloader = document.querySelector('.preloader')
 let imgWedding = document.getElementById('img_wedding')
 let i2 = 0
+let cardLow = document.querySelector('.card_low')
+let buyLow = document.querySelector('.buy_low')
+let cardMiddle = document.querySelector('.card_middle')
+let buyMiddle = document.querySelector('.buy_middle')
+let cardHigh = document.querySelector('.card_high')
+let buyHigh = document.querySelector('.buy_high')
 
 window.addEventListener('load', () => {
     preloader.classList.add('hide')
@@ -41,3 +47,33 @@ const loopWeddings = setInterval(() => {
         imgWedding.setAttribute('src', `../images/casamento_${weddingType}${i2}.jpg`)
     },
     1000)
+
+cardLow.addEventListener('click', () => {
+    cardLow.style.display = 'none'
+    buyLow.style.display = 'flex'
+})
+
+buyLow.addEventListener('click', () => {
+    buyLow.style.display = 'none'
+    cardLow.style.display = 'flex'
+})
+
+cardMiddle.addEventListener('click', () => {
+    cardMiddle.style.display = 'none'
+    buyMiddle.style.display = 'flex'
+})
+
+buyMiddle.addEventListener('click', () => {
+    buyMiddle.style.display = 'none'
+    cardMiddle.style.display = 'flex'
+})
+
+cardHigh.addEventListener('click', () => {
+    cardHigh.style.display = 'none'
+    buyHigh.style.display = 'flex'
+})
+
+buyHigh.addEventListener('click', () => {
+    buyHigh.style.display = 'none'
+    cardHigh.style.display = 'flex'
+})
