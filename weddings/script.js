b4d78f5725c249f4925e22033bfee35a7a73c711
RafaelHDSV <rafaebolt@gmail.com>
2022-12-12 22:10:35 -0300

    Updating weddings

--- weddings/script.js
+++ weddings/script.js
@@ -5,34 +5,33 @@ let i2 = 0
 window.addEventListener('load', () => {
     preloader.classList.add('hide')
     document.body.style.overflowY = 'visible'
-
-    imgWeddingCurrent = imgWedding.getAttribute('src')
 })
 
+imgWeddingCurrent = imgWedding.getAttribute('src')
+imgWeddingCurrent = imgWeddingCurrent.replace('../images/casamento_', '')
+imgWeddingCurrent = imgWeddingCurrent.replace('1.jpg', '')
+
+if (imgWeddingCurrent == 'praia') {
+    weddingType = 'praia'
+} else if (imgWeddingCurrent == 'igreja') {
+    weddingType = 'igreja'
+} else if (imgWeddingCurrent == 'eucaliptos') {
+    weddingType = 'eucaliptos'
+} else if (imgWeddingCurrent == 'noivas') {
+    weddingType = 'noivas'
+} else if (imgWeddingCurrent == 'moderno') {
+    weddingType = 'moderno'
+} else if (imgWeddingCurrent == 'livre') {
+    weddingType = 'livre'
+} else if (imgWeddingCurrent == 'campo') {
+    weddingType = 'campo'
+} else if (imgWeddingCurrent == 'fazenda') {
+    weddingType = 'fazenda'
+} else if (imgWeddingCurrent == 'vibrante') {
+    weddingType = 'vibrante'
+}
+
 const loopWeddings = setInterval(() => {
-        imgWeddingCurrent = imgWeddingCurrent.replace('../images/casamento_', '')
-        imgWeddingCurrent = imgWeddingCurrent.replace('1.jpg', '')
-
-        if (imgWeddingCurrent == 'praia') {
-            weddingType = 'praia'
-        } else if (imgWeddingCurrent == 'igreja') {
-            weddingType = 'igreja'
-        } else if (imgWeddingCurrent == 'eucaliptos') {
-            weddingType = 'eucaliptos'
-        } else if (imgWeddingCurrent == 'noivas') {
-            weddingType = 'noivas'
-        } else if (imgWeddingCurrent == 'moderno') {
-            weddingType = 'moderno'
-        } else if (imgWeddingCurrent == 'livre') {
-            weddingType = 'livre'
-        } else if (imgWeddingCurrent == 'campo') {
-            weddingType = 'campo'
-        } else if (imgWeddingCurrent == 'fazenda') {
-            weddingType = 'fazenda'
-        } else if (imgWeddingCurrent == 'vibrante') {
-            weddingType = 'vibrante'
-        }
-
         i2 += 1
 
         if (i2 == 5) {
@@ -41,4 +40,4 @@ const loopWeddings = setInterval(() => {
 
         imgWedding.setAttribute('src', `../images/casamento_${weddingType}${i2}.jpg`)
     },
-    3000)
+    1500)
